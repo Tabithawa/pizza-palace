6ab4843bf10f23ad0da342e259023c5fa9659c98
Tabithawa <tabitha.wainaina@student.moringaschool.com>
2021-06-27 13:26:43 +0300

    use switch case and find the total of the order

--- index.js
+++ index.js
@@ -8,3 +8,67 @@ $(document).ready(function(){
       var pizzaSize = $("#size option:selected").val();
       var pizzaCrust = $("#crust option:selected").val();
       var pizzaTopping = $("#topping option:selected").val();
+
+      switch(pizzaSize){
+        case "0":
+          price =0;
+        break;
+        case "Large":
+           price = 1500;
+           console.log(price);
+         break;
+         case "Medium":
+           price = 1000;
+           console.log("The price is "+price);
+         break;
+         case "Small":
+           price = 500;
+           console.log(price);
+         default:
+           console.log("error"); 
+       }
+       switch(pizzaCrust){
+          case "0":
+            crust_price = 0;
+          break;
+          case "Crispy":
+            crust_price = 50;
+          break;
+          case "Stuffed":
+            crust_price = 150;
+          break;
+          case "Gluten-Free":
+            crust_price = 200;
+          break;
+          default:
+            console.log("No price"); 
+        }
+        switch(pizzaTopping){
+          case "0":
+            topping_price = 0;
+          break;
+          case "Sausage":
+            topping_price = 50;
+          break;
+          case "Mushrooms":
+            topping_price = 50;
+          break;
+          case "Extra Cheese":
+            topping_price = 50;
+          break;
+          default:
+            console.log("No price"); 
+        }
+  
+      var total = price + crust_price + topping_price;
+      console.log(total);
+      let checkoutTotal =0;
+      checkoutTotal = checkoutTotal + total;
+  
+  
+      event.preventDefault();
+      document.getElementById("pizzaname").innerHTML = pizzaType;
+      document.getElementById("pizzasize").innerHTML = pizzaSize;
+      document.getElementById("pizzacrust").innerHTML = pizzaCrust;
+      document.getElementById("pizzatopping").innerHTML = pizzaTopping;
+      document.getElementById("totals").innerHTML = checkoutTotal;
